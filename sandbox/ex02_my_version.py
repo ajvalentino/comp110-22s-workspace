728ccd0211565816060bdf1a25980b9567e5165c
Anthony Valentino <avalentino@unc.edu>
2022-01-27 09:28:02 -0500

    Change sto my version

--- sandbox/ex02_my_version.py
+++ sandbox/ex02_my_version.py
@@ -6,39 +6,50 @@ WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
 
-secret: str = "knoll"
-guess: str = input(f"What is your {len(secret)}-letter guess? ")
+secret: str = "test"
 i: int = 0
 checks: int = 0
 somewhere: bool = False
 result: str = ""
+success: bool = False
 
-while len(guess) != len(secret):
-    guess = input(f"That was not {len(secret)} letters! Try again: ")
+while not success:
+    guess: str = input(f"What is your {len(secret)}-letter guess? ")
 
-while i < len(secret):
-    if guess[i] == secret[i]:
-        result = result + GREEN_BOX
-        somewhere = True
-    else:
-        while checks < len(secret):
-            if guess[i] == secret[checks]:
-                result = result + YELLOW_BOX
-                somewhere = True
-                checks = len(secret)
-            checks = checks + 1
+    if guess == "exit":
+        exit()
+
+    while len(guess) != len(secret):
+        guess = input(f"That was not {len(secret)} letters! Try again: ")
+
+    while i < len(secret):
+        if guess[i] == secret[i]:
+            result = result + GREEN_BOX
+            somewhere = True
+        else:
+            while checks < len(secret):
+                if guess[i] == secret[checks]:
+                    result = result + YELLOW_BOX
+                    somewhere = True
+                    checks = len(secret)
+                checks = checks + 1
+            
+            checks = 0
         
-        checks = 0
-    
-    if not somewhere:
-        result = result + WHITE_BOX
-    
-    i = i + 1
-    somewhere = False
+        if not somewhere:
+            result = result + WHITE_BOX
+        
+        i = i + 1
+        somewhere = False
 
-print(result)
+    print(result)
 
-if guess == secret:
-    print("Woo! You got it!")
-else:
-    print("Not quite. Play again soon!")
+    if guess == secret:
+        print("Woo! You got it!")
+        success = True
+    else:
+        print("Not quite. Try again or exit!")
+        result = ""
+        i = 0
+
+print("Play again sometime!")
