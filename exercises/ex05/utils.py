a195add7857d66138a5875dd9bc99a42937a7dfc
Anthony Valentino <avalentino@unc.edu>
2022-02-20 13:14:25 -0500

    Changes

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -4,6 +4,7 @@ __author__ = "730466997"
 
 
 def only_evens(numbers: list[int]) -> list[int]:
+    """Given a list of integers this function returns the even numbers."""
     even_numbers: list[int] = []
     for number in numbers:
         if number % 2 == 0:
@@ -12,6 +13,7 @@ def only_evens(numbers: list[int]) -> list[int]:
 
 
 def sub(numbers: list[int], start: int, end: int) -> list[int]:
+    """Given a list and start/end indices this function returns a subset of the list."""
     sublist: list[int] = []
     i: int = 0
     for number in numbers:
@@ -22,6 +24,7 @@ def sub(numbers: list[int], start: int, end: int) -> list[int]:
 
 
 def concat(first_list: list[int], second_list: list[int]) -> list[int]:
+    """Given two lists this function concatenates them together."""
     concat_list: list[int] = []
     for number in first_list:
         concat_list.append(number)
